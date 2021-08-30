5c2d49fa1d9d64159d2011ef48b1373333845d03
Cisco <895275020@qq.com>
2021-08-30 17:51:55 +0800

    'initproject'

---
+++ src/main/java/io/github/sunshine001/guava/GuavaApplication.java
@@ -0,0 +1,13 @@
+package io.github.sunshine001.guava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/sunshine001/guava/GuavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sunshine001.guava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
