14dfd7d35c82170dbf00e87e33c0e293fa3728bf
Cisco <895275020@qq.com>
2021-09-02 11:07:03 +0800

    'init'

---
+++ src/main/java/io/github/sunshine001/guava/Application.java
@@ -0,0 +1,7 @@
+package io.github.sunshine001.guava;
+
+public class Application {
+    public static void main(String[] args) {
+        Console.log("Hello World", "cisco");
+    }
+}
--- src/main/java/io/github/sunshine001/guava/Console.java
+++ src/main/java/io/github/sunshine001/guava/Console.java
@@ -1,6 +1,13 @@
 package io.github.sunshine001.guava;
 
+/**
+ * package println for quickly development
+ */
 public class Console {
+    /**
+     * normal print
+     * @param args
+     */
     public static void log(Object ...args) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < args.length; i++) {
